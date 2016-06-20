e12d82d243e0a2fcdcb9e9f3638bf608fa85d7a2
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2016-06-20 09:53:19 -0300

    Update task sass

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -6,11 +6,18 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var gutil = require('gulp-util');
 
 // tasks
 gulp.task('sass', function () {
   gulp.src('../assets/sass/**/*.scss')
+
+    .pipe(plumber(function(error) {
+        gutil.log(gutil.colors.red(error.message));
+        this.emit('end');
+    }))
+
     .pipe(sass().on('Erro de Sintaxe', sass.logError))
     .pipe(gulp.dest('../assets/css/'));
 });
-
