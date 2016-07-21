ccf5709badb00953e0d27653417bd5928daa8b62
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2016-07-21 19:22:04 -0300

    Insere fancybox

--- gulp/tasks/concatjs.js
+++ gulp/tasks/concatjs.js
@@ -8,25 +8,28 @@ var concat = require('gulp-concat');
 gulp.task('concatjs', function() {
 	return gulp.src([
   		
-  		// bootstrap	
-    	'../assets/js/bootstrap/affix.js',
-    	'../assets/js/bootstrap/alert.js',
-    	'../assets/js/bootstrap/button.js',
-    	'../assets/js/bootstrap/carousel.js',
-    	'../assets/js/bootstrap/collapse.js',
-    	'../assets/js/bootstrap/dropdown.js',
-    	'../assets/js/bootstrap/modal.js',
-    	'../assets/js/bootstrap/popover.js',
-    	'../assets/js/bootstrap/scrollspy.js',
-    	'../assets/js/bootstrap/tab.js',
-    	'../assets/js/bootstrap/tooltip.js',
-    	'../assets/js/bootstrap/transition.js',
+        // BOOTSTRAP    
+        '../assets/js/bootstrap/affix.js',
+        '../assets/js/bootstrap/alert.js',
+        '../assets/js/bootstrap/button.js',
+        //'../assets/js/bootstrap/carousel.js',
+        '../assets/js/bootstrap/collapse.js',
+        '../assets/js/bootstrap/dropdown.js',
+        '../assets/js/bootstrap/modal.js',
+        // '../assets/js/bootstrap/popover.js', Esse sapeca está com treta :(
+        '../assets/js/bootstrap/scrollspy.js',
+        '../assets/js/bootstrap/tab.js',
+        '../assets/js/bootstrap/tooltip.js',
+        '../assets/js/bootstrap/transition.js',
 
-    	// owl carousel
-    	'../assets/js/owl-carousel/owl.carousel.js',
+        // FANCYBOX
+        '../assets/js/fancybox/jquery.fancybox.js',        
 
-    	// functions
-    	'../assets/js/functions/functions.js'
+        // OWL CAROUSEL
+        '../assets/js/owl-carousel/owl.carousel.js',
+
+        // FUNCTIONS
+        '../assets/js/functions/functions.js'
   ])
     .pipe(concat('all.js'))
     .pipe(gulp.dest('../assets/js/'));
