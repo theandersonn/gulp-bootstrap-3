214eb50154d21b3a2b995b7ed7d1aaa5fd2188af
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2017-05-31 17:21:34 -0300

    Update task sass com gulp-notify

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -4,22 +4,14 @@
 // plugins
 'use strict';
 
-var gulp 	= require('gulp');
-var prefix 	= require('gulp-autoprefixer');
-var sass 	= require('gulp-sass');
-var plumber = require('gulp-plumber');
-var gutil 	= require('gulp-util');
+var gulp 		= require('gulp');
+var sass 		= require('gulp-sass');
+var notify 		= require("gulp-notify");
 
 // tasks
 gulp.task('sass', function () {
   gulp.src('../assets/sass/**/*.scss')
-
-    .pipe(plumber(function(error) {
-        gutil.log(gutil.colors.red(error.message));
-        this.emit('end');
-    }))
- 
-    .pipe(sass().on('Erro de Sintaxe', sass.logError))
-    .pipe(prefix('last 4 versions'))
-    .pipe(gulp.dest('../assets/css/'));
+    .pipe(sass())
+    .on("error", notify.onError({title:"Ai, tu quebrou o SASS!"}))
+    .pipe(gulp.dest('../assets/css/'))
 });
