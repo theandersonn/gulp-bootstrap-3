3f0ccae0f011320c5695353be98d4042faf9de66
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2017-07-07 14:22:46 -0300

    Update task css

--- gulp/tasks/cssmin.js
+++ gulp/tasks/cssmin.js
@@ -7,7 +7,7 @@ var cssmin 	= require('gulp-cssmin');
 var rename 	= require('gulp-rename');
 
 gulp.task( 'cssmin', function() {
-  gulp.src('../src/css/all.css')
+  gulp.src('../static/css/all.css')
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('../static/css/'));
